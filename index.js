a57370331c75ae304f4b03477942ff34ad7107f7
akkiiSingh <akshat0703.singh>
2024-12-11 11:15:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Write your code below:
+let ele = document.getElementsByTagName('li');
+ele[4].style.color = 'blue';
+
+
+for (let i = 0; i < ele.length; i++){
+    ele[i].style.fontStyle = 'italic';
+}
